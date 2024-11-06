27d1380120b349d99ff6b0a823fc0316491c462d
orsoli <fljcoli@gmail.com>
2024-11-06 18:01:13 +0100

    Added details in types table in db

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,35 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = [
+            [
+                'title' => 'Front-end',
+                'description' => 'Projects focused on the client-side, involving UI/UX and interaction design.'
+            ],
+            [
+                'title' => 'Back-end',
+                'description' => 'Projects focused on server-side functionality, database management, and APIs.'
+            ],
+            [
+                'title' => 'Full-stack',
+                'description' => 'Projects that include both front-end and back-end development.'
+            ],
+            [
+                'title' => 'Database',
+                'description' => 'Projects centered on database architecture, optimization, and data management.'
+            ],
+            [
+                'title' => 'Mobile',
+                'description' => 'Projects aimed at developing applications for mobile platforms such as iOS and Android.'
+            ],
+            [
+                'title' => 'Devops',
+                'description' => 'Projects that focus on automation, infrastructure management, and CI/CD pipelines.'
+            ],
+        ];
+
+        foreach($types as $type){
+            $type = Type::create($type);
+        }
     }
 }
