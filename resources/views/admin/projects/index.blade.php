e4925ce3e7b6dd4c7421b0c788a90b4aa39b6170
orsoli <fljcoli@gmail.com>
2024-11-07 00:17:14 +0100

    Modify projects view

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,26 +2,45 @@
 
 @section('content')
 <div class="container py-5">
-    <h1 class="text-center py-3">Projects</h1>
-    <div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">
-        @forelse ($projects as $project)
-        <div class="col">
-            <div class="card shadow">
-                <div class="card-body">
-                    <h5 class="card-title">{{$project->name}}</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">{{$project->type->title}}
-                    </h6>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">Project Manager: {{$project->project_manager}}
-                    </h6>
-                    <p class="card-text">{{$project->description}}</p>
-                    <a href="#" class="card-link">Project link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
-            </div>
-        </div>
-        @empty
-        <h3>You have no projects in DB</h3>
-        @endforelse
-    </div>
+    <!-- Create projects Button-->
+    {{-- <a href="{{route('projects.create')}}" type="button" class="btn btn-sm btn-outline-primary mb-3">
+        Create new Project
+    </a> --}}
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Type</th>
+                <th scope="col">Manager</th>
+                <th scope="col">Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($projects as $project)
+            <tr>
+                <th scope="row">{{$project->id}}</th>
+                <td>{{$project->name}}</td>
+                <td>{{$project->type->title}}</td>
+                <td>{{$project->project_manager}}</td>
+                <td>{{$project->description}}</td>
+                {{-- <td>
+                    <a href="{{route('projects.show', $projects->id)}}" class="btn btn-sm btn-success"><i
+                            class="bi bi-eye-fill"></i></a>
+                    <a href="{{route('projects.edit', $projects->id)}}" class="btn btn-sm btn-warning"><i
+                            class="bi bi-pencil-fill"></i></a>
+                    <form action="{{route('projects.destroy', $projects->id)}}" method="POST" class="d-inline">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-sm btn-danger delete-btn" type="submit" value="delete"
+                            delete-data-name="{{$projects->name}}"><i class="bi bi-trash3-fill"></i></button>
+                    </form>
+                </td> --}}
+            </tr>
+            @empty
+            abort(404)
+            @endforelse
+        </tbody>
+    </table>
 </div>
 @endsection
